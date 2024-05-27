e781d50b92ead31ef0d30621d9f5251d950e55b6
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-05-27 10:05:08 +1000

    Prompt to init git repo

--- .init-script/index.js
+++ .init-script/index.js
@@ -1,39 +1,15 @@
-import fs from "node:fs/promises";
+import fs from "node:fs";
 import path from "node:path";
 import url from "node:url";
+import cp from "node:child_process";
+
 import chalk from "chalk";
 import prompts from "prompts";
 import validatePackageName from "validate-npm-package-name";
 
 const initScriptDir = path.dirname(url.fileURLToPath(import.meta.url));
 const rootDir = path.resolve(initScriptDir, "..");
-
-async function updateRootPackage(updateFn) {
-  const pkgPath = path.join(rootDir, "package.json");
-  const pkg = JSON.parse(await fs.readFile(pkgPath, "utf-8"));
-  const updatedPkg = updateFn(pkg);
-  await fs.writeFile(
-    pkgPath,
-    JSON.stringify(updatedPkg ?? pkg, null, 2) + "\n"
-  );
-}
-
-async function detectPackageManager() {
-  const detect = (name, file) =>
-    fs
-      .access(path.join(rootDir, file))
-      .then(() => name)
-      .catch(() => null);
-
-  return (
-    (await detect("pnpm", "pnpm-lock.yaml")) ??
-    (await detect("yarn", "yarn.lock")) ??
-    (await detect("bun", "bun.lockb")) ??
-    "npm"
-  );
-}
-
-const packageManager = await detectPackageManager();
+const packageManager = detectPackageManager();
 
 console.log(
   chalk.blue(
@@ -46,27 +22,42 @@ console.log(
   )
 );
 
-const { projectName } = await prompts({
-  type: "text",
-  name: "projectName",
-  message: "What would you like to name your project?",
-  initial: path.basename(rootDir),
-  validate: (name) => {
-    const { validForNewPackages, errors } = validatePackageName(name);
-    if (!validForNewPackages) {
-      return errors.join("\n");
-    }
-    return true;
+const { projectName, gitInit } = await prompts([
+  {
+    type: "text",
+    name: "projectName",
+    message: "What would you like to name your project?",
+    initial: path.basename(rootDir),
+    validate: (name) => {
+      const { validForNewPackages, errors } = validatePackageName(name);
+      if (!validForNewPackages) {
+        return errors.join("\n");
+      }
+      return true;
+    },
   },
-});
+  {
+    type: fsExists(".git") ? null : "confirm",
+    name: "gitInit",
+    message: "Would you like to initialize a Git repository?",
+    initial: true,
+    validate: (name) => {
+      const { validForNewPackages, errors } = validatePackageName(name);
+      if (!validForNewPackages) {
+        return errors.join("\n");
+      }
+      return true;
+    },
+  },
+]);
 
-// Update project name in package.json
-updateRootPackage((pkg) => {
+// Add project name to package.json
+modifyRootPackage((pkg) => {
   pkg.name = projectName;
 });
 
-// Update readme so it's no longer a template
-await fs.writeFile(
+// Replace template instructions in readme with project instructions
+fs.writeFileSync(
   "../README.md",
   [
     `# ${projectName}`,
@@ -86,9 +77,49 @@ await fs.writeFile(
 );
 
 // Clean up the init script
-await fs.rm(initScriptDir, { recursive: true });
-updateRootPackage((pkg) => {
+console.log(chalk.blue(`\nCleaning up init script...`));
+fs.rmSync(initScriptDir, { recursive: true });
+console.log(chalk.blue(`Cleaning up package.json scripts...`));
+modifyRootPackage((pkg) => {
   delete pkg.scripts.postinstall;
 });
 
+if (gitInit) {
+  console.log(chalk.blue(`Initializing Git repository...`));
+
+  const spawnOpts = { cwd: rootDir, stdio: "inherit" };
+  cp.spawnSync("git", ["init"], spawnOpts);
+  cp.spawnSync("git", ["add", "."], spawnOpts);
+  cp.spawnSync("git", ["commit", "-m", "Initial commit"], spawnOpts);
+}
+
 console.log(chalk.green(`\n✔ Template init script finished successfully`));
+
+/*
+ * Helper functions
+ */
+
+function fsExists(file) {
+  try {
+    fs.accessSync(path.resolve(rootDir, file));
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function detectPackageManager() {
+  return (
+    (fsExists("pnpm-lock.yaml") && "pnpm") ??
+    (fsExists("yarn.lock") && "yarn") ??
+    (fsExists("bun.lockb") && "bun") ??
+    "npm"
+  );
+}
+
+function modifyRootPackage(updateFn) {
+  const pkgPath = path.join(rootDir, "package.json");
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
+  const updatedPkg = updateFn(pkg);
+  fs.writeFileSync(pkgPath, JSON.stringify(updatedPkg ?? pkg, null, 2) + "\n");
+}
